ec25205a62a521c741f2d30389e3012caa7e94a3
Hol9 <sigiloso1342@gmail.com>
2026-07-06 23:06:14 -0400

    Update LirikPermadeath.kt

--- src/main/kotlin/cl/lirikworks/lirikpermadeath/LirikPermadeath.kt
+++ src/main/kotlin/cl/lirikworks/lirikpermadeath/LirikPermadeath.kt
@@ -1,4 +1,34 @@
 package cl.lirikworks.lirikpermadeath
 
-class LirikPermadeath {
+import net.neoforged.bus.api.IEventBus
+import net.neoforged.fml.ModContainer
+import net.neoforged.fml.common.Mod
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+const val MOD_ID = "lirikpermadeath"
+
+@Mod(MOD_ID)
+class LirikPermadeath(
+    modBus: IEventBus,
+    modContainer: ModContainer
+) {
+    init {
+        LOGGER.info("Inicializando Lirik Permadeath")
+
+        registerContent(modBus)
+        registerConfig()
+    }
+
+    private fun registerContent(modBus: IEventBus) {
+
+    }
+
+    private fun registerConfig() {
+
+    }
+
+    companion object {
+        val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
+    }
 }
